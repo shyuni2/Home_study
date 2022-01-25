911cf58815a363f829154a72533a887622f64b12
이지구 <rose97_@naver.com>
2022-01-25 20:46:35 +0900

    up

--- code/ThreadChat_client/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <WinSock2.h>
-#include <conio.h>
-#include "Packet.h"
-#pragma comment (lib,"ws2_32.lib")
-
-HANDLE hConnectEvent; // 연결됐을때 이벤트
-HANDLE hExecuteSemaphore; // 세마포어
-
