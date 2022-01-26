099bd85d76316c79cc026fe8db45db4d21c4eb83
이지구 <rose97_@naver.com>
2022-01-26 14:43:30 +0900

    up

--- code/sampleWin/main.h
+++ code/sampleWin/main.h
@@ -1,7 +1,22 @@
 #pragma once
+#include "AsyncSelect.h"
 #include "Core.h"
 
 class main : public Core
 {
+	AsyncSelect m_Net;
+	HWND m_hEdit;
+	HWND m_hButton;
+	HWND m_hUserChatBox;
+	int m_MyChatBox;
+public:
+	virtual bool Init() override;
+	virtual bool Frame() override;
+	virtual bool Render() override;
+	virtual bool Release() override;
+	virtual LRESULT MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+public:
+	main();
+	~main();
 };
 
